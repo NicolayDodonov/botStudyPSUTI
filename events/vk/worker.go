1120ae072c3e8eb704ce359d3b2e54a09ef78a7d
NicolayDodonov <95965202+NicolayDodonov@users.noreply.github.com>
2024-09-21 13:55:11 +0400

    start make vk-worker

--- events/vk/worker.go
+++ events/vk/worker.go
@@ -1,1 +1,24 @@
 package vk
+
+import (
+	"BotStudyPSUTI/client/vk"
+	"BotStudyPSUTI/events"
+)
+
+type Worker struct {
+	vk *vk.Client
+}
+
+func New(client *vk.Client) Worker {
+	return Worker{
+		vk: client,
+	}
+}
+
+func (w *Worker) Fetch(limit int) ([]events.Event, error) {
+	return nil, nil
+}
+
+func (w *Worker) Process(e events.Event) error {
+	return nil
+}
